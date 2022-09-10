f1110d982c13d63fd59f402ab1aafcdc6e85f440
Anastasiya Saakyan <vv999vv@bk.ru>
2022-09-10 16:03:58 +0300

    Initial commit

---
+++ src/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    private String name;
+    private String surName;
+    private int quantityTickets;
+
+    public Person(String name, String surName, int quantityTickets) {
+        this.name = name;
+        this.surName = surName;
+        this.quantityTickets = quantityTickets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public int getQuantityTickets() {
+        return quantityTickets;
+    }
+
+    public void setQuantityTickets(int quantityTickets) {
+        this.quantityTickets = quantityTickets;
+    }
+}
